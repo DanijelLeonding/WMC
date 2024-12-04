ef6f3da91a4352b4792d7184b60ac46d19a51412
Danijel Peric <d.peric@students.htl-leonding.ac.at>
2024-12-04 04:00:09 +0100

    Task 4 (stable): using cowsay

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ console.log("Hello World");
 import chalk from 'chalk';
 
 console.log(chalk.bgRed.white('Hello DanijelLeonding'));
+
+import cowsay from "cowsay";
+
+// Lässt die Kuh "Moo" sagen
+console.log(cowsay.say({ text: "Moo" }));
+
+// Lässt die Kuh deinen Namen "denken"
+console.log(cowsay.think({ text: "DanijelLeonding" }));
